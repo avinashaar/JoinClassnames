f94b5e4a78f888f2d10f2ecf631c9f5b937d1562
aarm <avinashmaurya84@gmail.com>
2025-02-10 22:54:48 +0530

    add function

---
+++ index.js
@@ -0,0 +1,30 @@
+const JoinClassNames = (...args) => {
+  let classnameList = [];
+  args.forEach((classname) => {
+    if (!classname) {
+      return;
+    }
+
+    let classnameType = typeof classname;
+    if (classnameType === "string" || classnameType === "number") {
+      classnameList.push(classname);
+      return;
+    }
+
+    if (Array.isArray(classname)) {
+      classnameList.push(JoinClassNames(...classname));
+      return;
+    }
+
+    if (classname === "object") {
+      for (let key in classname) {
+        if (Object.hasOwn(classname, key) && classname[key]) {
+          classnameList.push(key);
+        }
+      }
+      return;
+    }
+  });
+
+  return classnameList.join(" ");
+};
